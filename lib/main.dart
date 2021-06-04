ab0b6d4de5d38de6bac72dee47c236dad1904386
FNSY <fadynabil96@gmail.com>
2021-06-04 14:45:01 +0200

    Added routing.

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,58 @@
+import 'dart:io';
+
 import 'package:actors_viewer/home_screen/home_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+import 'app_model.dart';
+import 'constants/routes.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(App(appModel: AppModel(),));
 }
-
-class MyApp extends StatelessWidget {
+final GlobalKey<NavigatorState> navigatorKey = new GlobalKey<NavigatorState>();
+final RouteObserver<PageRoute> routeObserver = new RouteObserver<PageRoute>();
+class App extends StatelessWidget {
   // This widget is the root of your application.
+  final AppModel appModel;
+
+  const App({@required this.appModel});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return ScopedModel<AppModel>(
+      model: appModel,
+      child: ScopedModelDescendant<AppModel>(
+        builder: (context, child, model) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            // initialRoute: kIsWeb ? ScreenRoutes.LOGIN_SCREEN : ScreenRoutes.SPLASH_SCREEN,
+            onGenerateRoute: _getRoute,
+            navigatorKey: navigatorKey,
+            navigatorObservers: [routeObserver],
+          );
+        },
       ),
-      home: HomeScreen(),
     );
   }
+}
+
+Route _getRoute(RouteSettings settings) {
+  switch (settings.name) {
+    default:
+      return _buildRoute(settings, HomeScreen());
+  }
+}
+
+_buildRoute(RouteSettings settings, Widget page) {
+  return Platform.isAndroid
+      ? PageRouteBuilder(
+          settings: settings,
+          pageBuilder: (BuildContext context, _, __) => page,
+        )
+      : CupertinoPageRoute(
+          builder: (BuildContext context) => page,
+          settings: settings,
+        );
 }
